4dd2fade769cac2199f830d7713628ff19a68487
Mona Lisa <38268201+MatKurianski@users.noreply.github.com>
2018-10-29 19:18:02 -0300

    versão 2.0

--- trabalho.cpp
+++ trabalho.cpp
@@ -1,4 +1,6 @@
 #include "ep.h"
+#define B -1
+#define E -2
 
 // ######### ESCREVA O NUMERO DO SEU GRUPO AQUI, CONFORME LINK NA ESPECIFICACAO DO EP
 // ignore os warning to compilador, se houver
@@ -38,6 +40,23 @@ NO* push (NO* *lista, int tipo, int ch) {
     return resp;
 }
 
+NO* create(int* chs, int j, int n) {
+    NO* p = NULL;
+    int i = j;
+    for (i; i < n; i++) {
+        //printf("%i\n", chs[i]);
+        if (chs[i] == -1) {
+            NO* psub = push(&p, 2, -10);
+            psub->sublista = create(chs, i+1, n);
+            while (chs[i] != -2) i++;
+            chs[i] = NULL;
+        } else if (chs[i] == -2 && j != 0) {
+            return p;
+        } else push(&p, 1, chs[i]);
+    }
+    return p;
+}
+
 NO* duplicate (NO* p) {
     NO* resp = NULL;
     if (p) {
@@ -50,17 +69,33 @@ NO* duplicate (NO* p) {
     return resp;
 }
 
-NO* getUltLin(NO* atual) {
+NO* getUltLin(NO* atual, bool requireCh) {
     NO* ult = NULL;
     while (atual) {
-        ult = atual;
+        if (requireCh)
+            if(atual->tipo == 1) ult = atual;
+        else ult = atual;
         atual = atual->prox;
     }
+    if(ult) printf("%i", ult->tipo);
     return ult;
+}
+
+void exibir(NO* p) {
+    while (p) {
+        if (p->tipo == 1) printf("%i ", p->chave);
+        else {
+            printf("[sub] ");
+            exibir(p->sublista);
+            printf("[fim_sub]");
+        }
+        p = p-> prox;
+    }
 }
 
 // o EP consiste em implementar esta funcao
 NO* listarChaves(NO* entrada) {
+    //printf("Chamada!\n");
     NO* atual = entrada;
     NO* resp = NULL;
     NO* inicio = NULL;
@@ -77,16 +112,29 @@ NO* listarChaves(NO* entrada) {
         atual = atual->prox;
     }
 
-    if (inicio == NULL) inicio = entrada;
-    NO* ult = getUltLin(inicio);
+    NO* ult = getUltLin(inicio, true);
+
+
     atual = entrada;
+
+    if (ult == NULL) {
+        inicio = duplicate(atual->sublista);
+        NO* aux = inicio;
+        atual = atual->prox;
+        while (atual) {
+            aux->prox = duplicate(atual->sublista);
+            atual = atual->prox;
+            aux = aux->prox;
+        }
+    }
+
 
     int contador = 0;
 
     while(atual) {
         if (atual->tipo == 2) {
-            contador++;
             ult->prox = duplicate(atual->sublista);
+            contador++;
             while (ult->prox) ult = ult->prox;
         }
         atual = atual->prox;
@@ -95,13 +143,7 @@ NO* listarChaves(NO* entrada) {
     return inicio;
 }
 
-void exibir(NO* p) {
-    while (p) {
-        if (p->tipo == 1) printf("%i ", p->chave);
-        else exibir(p->sublista);
-        p = p-> prox;
-    }
-}
+
 
 
 //---------------------------------------------------------
@@ -131,10 +173,17 @@ int main() {
     NO* sub7 = push(&teste, 2, -1);
         push(&sub7->sublista, 1, 7);
     push(&teste, 1, 4);
+    NO* debug = push(&teste, 2, 5);
+    push(&debug->sublista, 1, 12);
 
-    exibir(teste);
+    int a[] = {B, 5 ,E, B, 5, E, B, 5, E, B, 5, E};
+
+    size_t n = sizeof(a)/sizeof(a[0]);
+    NO* teste3 = create(a, 0, n);
+
+    exibir(teste3);
     printf("\n");
-    exibir(listarChaves(teste));
+    exibir(listarChaves(teste3));
 }
 
 // por favor nao inclua nenhum código abaixo da função main()
